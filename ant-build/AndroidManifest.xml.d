143ff2e24f3c5fb8e6ecfc639d413e83743e6e2c
hoozi <287036406@qq.com>
2015-02-27 15:52:19 +0800

    Revert "Revert "Add New""
    This reverts commit 5a90a79fad2cb50fc96f9497f2fd7167d80e4f92.

---
+++ ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\yibutong-mobile\platforms\android\ant-build\AndroidManifest.xml : \
+D:\yibutong-mobile\platforms\android\AndroidManifest.xml \
---
+++ ant-build/classes.dex.d
@@ -0,0 +1,13 @@
+D:\yibutong-mobile\platforms\android\ant-build\classes.dex : \
+D:\yibutong-mobile\platforms\android\ant-build\classes\org\apache\cordova\device\Device.class \
+D:\yibutong-mobile\platforms\android\ant-build\classes\org\apache\cordova\networkinformation\NetworkManager$1.class \
+D:\yibutong-mobile\platforms\android\ant-build\classes\org\apache\cordova\networkinformation\NetworkManager.class \
+D:\yibutong-mobile\platforms\android\ant-build\classes\org\apache\cordova\splashscreen\SplashScreen.class \
+D:\yibutong-mobile\platforms\android\ant-build\classes\yibutong\mobile\BuildConfig.class \
+D:\yibutong-mobile\platforms\android\ant-build\classes\yibutong\mobile\R$attr.class \
+D:\yibutong-mobile\platforms\android\ant-build\classes\yibutong\mobile\R$drawable.class \
+D:\yibutong-mobile\platforms\android\ant-build\classes\yibutong\mobile\R$string.class \
+D:\yibutong-mobile\platforms\android\ant-build\classes\yibutong\mobile\R$xml.class \
+D:\yibutong-mobile\platforms\android\ant-build\classes\yibutong\mobile\R.class \
+D:\yibutong-mobile\platforms\android\ant-build\classes\yibutong\mobile\yibutong.class \
+D:\yibutong-mobile\platforms\android\ant-build\dexedLibs\classes-0e69adcdbd646e1026ed41b0a51e5a27.jar \
